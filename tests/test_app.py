d433363b911b2454313e51ad59b8f4456ded14c9
Saniya Parasara <saniyaparasara@gmail.com>
2025-08-26 06:58:31 +0530

    Fix Jenkinsfile git syntax

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,11 @@
-from app import app
+import sys, os
+
+# Add project root (where app.py is) to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import app   # Now Python can find app.py
 
 def test_home():
-    # create a test client
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
